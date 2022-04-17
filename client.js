a1188b412bc5b6fa512de820c95fdd53616606dc
J <joras.custodio@gmail.com>
2022-04-17 15:51:43 -0300

    now saves answers in a dict + foorloop to send them all in the end, abd 1 more question done

--- client.js
+++ client.js
@@ -21,47 +21,38 @@ axios.post('https://tecweb-js.insper-comp.com.br/token ',{
           }
         }).then((response) => {//console.log(response.data) 
           //response.data.ex.entrada
-
+           var dict = {}; 
           //------------------------//
           console.log(response.data.soma) 
           soma = response.data.soma.entrada.a + response.data.soma.entrada.b
           console.log(soma)
-          axios.post('https://tecweb-js.insper-comp.com.br/exercicio/soma',
-            {"resposta" : soma},
-            {headers:{
-              'Content-Type' : 'application/json',
-              'Accept' : 'application/json',
-              'Authorization' : 'Bearer ' + t
-            }}
-          ).then((response) => {//console.log(response.data)
-          })
-
+          dict["soma"] = soma
           //------------------------//
           console.log(response.data['tamanho-string'])
           length = response.data['tamanho-string'].entrada.string.length
           console.log(length)
-          axios.post('https://tecweb-js.insper-comp.com.br/exercicio/tamanho-string',
-            {"resposta" : length},
+          dict["tamanho-string"] = length
+          //------------------------//
+          console.log(response.data['nome-do-usuario'])
+          email = (response.data['nome-do-usuario'].entrada.email)
+          usuario = email.slice(0, email.search("@"))
+          dict["nome-do-usuario"] = usuario
+          //------------------------//
+          
+
+
+
+
+
+          for (const [key, value] of Object.entries(dict)) {
+            axios.post('https://tecweb-js.insper-comp.com.br/exercicio/' + key,
+            {"resposta" : value},
             {headers:{
               'Content-Type' : 'application/json',
               'Accept' : 'application/json',
               'Authorization' : 'Bearer ' + t
-            }}
-          ).then((response) => {//console.log(response.data)
-          })
-
-          //------------------------//
-          console.log(response.data['nome-do-usuario'])
-          console.log(response.data['nome-do-usuario'].entrada.email)
-          // console.log(length)
-          // axios.post('https://tecweb-js.insper-comp.com.br/exercicio/tamanho-string',
-          //   {"resposta" : length},
-          //   {headers:{
-          //     'Content-Type' : 'application/json',
-          //     'Accept' : 'application/json',
-          //     'Authorization' : 'Bearer ' + t
-          //   }}
-          // ).then((response) => {//console.log(response.data)
-          // })
+              }}
+            ).then((response) => {console.log(response.data)})
+          }
         });
     });
